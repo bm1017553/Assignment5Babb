98187239e5846d9fcec7de944210ca5948a88e22
bm1017553 <73726121+bm1017553@users.noreply.github.com>
2022-02-11 17:00:58 -0600

    okay not really

--- GroceryDelivery/Models/unit-testing-using-dotnet-test/CustomerDatabase.Tests/CustomerDatabase.ReturnCustomerName.cs
+++ GroceryDelivery/Models/unit-testing-using-dotnet-test/CustomerDatabase.Tests/CustomerDatabase.ReturnCustomerName.cs
@@ -4,22 +4,21 @@ using Xunit;
 
 namespace CustomerDatabase.Tests
 {
-    public class DbTest_IsDbReturn
+    public class ReturnCustomerName
     {
         [Theory]
         [InlineData(Customer.LastName)]
-        public void CustomerLastNameReturn(Customer.LastName?)
+        public void CustomerLastNameReturn(Models.Customer.LastName?)
         {
 
             if (Customer.LastName == Customer.LastName)
             {
                 return true;
             }
-            
+
             var result = _dbResult.Return(value);
 
             Assert.True(result, value);
-}
         }
     }
 }
